86384b8af5b0fc0b63c1ecc457982fd100117508
Fabian Heller <heller.fabian@gmail.com>
2018-06-22 21:24:23 +0800

    Fixed decoding

--- Makefile
+++ Makefile
@@ -9,6 +9,13 @@ libwebp.js: emsdk libwebp
 		-I libwebp \
 		webp.c \
 		libwebp/src/{dec,dsp,demux,enc,mux,utils}/*.c
+wasm: emsdk libwebp
+	source emsdk/emsdk_env.sh && emcc -Oz --closure 1 --llvm-lto 1 -o libwebp-wasm.js \
+		-s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s MODULARIZE=1 -s 'EXPORT_NAME="LibWebP"' \
+		-s ALLOW_MEMORY_GROWTH=1 -s ENVIRONMENT=web -s NO_FILESYSTEM=1 \
+		-I libwebp \
+		webp.c \
+		libwebp/src/{dec,dsp,demux,enc,mux,utils}/*.c
 emsdk:
 	git clone --depth 1 https://github.com/juj/emsdk.git
 	cd emsdk && ./emsdk install latest && ./emsdk activate latest
--- webp.c
+++ webp.c
@@ -3,43 +3,36 @@
 #include "src/webp/encode.h"
 #include "src/webp/decode.h"
 
-int result[2];
 EMSCRIPTEN_KEEPALIVE
-void encode(uint8_t* img_in, int width, int height, float quality) {
-  uint8_t* img_out;
-  size_t size;
+uint32_t *encode(uint8_t *img_in, int width, int height, float quality)
+{
+  uint32_t *result = malloc(sizeof(uint32_t) * 2);
+  uint8_t *img_out;
 
-  size = WebPEncodeRGBA(img_in, width, height, width * 4, quality, &img_out);
-
-  result[0] = (int)img_out;
-  result[1] = size;
+  result[1] = WebPEncodeRGBA(img_in, width, height, width * 4, quality, &img_out);
+  result[0] = (uint32_t)img_out;
+  return result;
 }
+
 EMSCRIPTEN_KEEPALIVE
-void decode(uint8_t* input_data, size_t input_size, uint8_t* output_buffer, size_t output_buffer_size, size_t output_stride) {
+int get_image_size(const uint8_t *img_in, size_t buffer_size, int *dimensions)
+{
+  return WebPGetInfo(img_in, buffer_size, dimensions, dimensions + 1);
+}
+
+EMSCRIPTEN_KEEPALIVE
+void decode(uint8_t *input_data, size_t input_size, uint8_t *output_buffer, size_t output_buffer_size, size_t output_stride)
+{
   WebPDecodeRGBAInto(input_data, input_size, output_buffer, output_buffer_size, output_stride);
 }
 
 EMSCRIPTEN_KEEPALIVE
-void free_result(uint8_t* result) {
+void free_result(uint8_t *result)
+{
   WebPFree(result);
 }
 
-EMSCRIPTEN_KEEPALIVE
-int get_result_pointer() {
-  return result[0];
-}
-
-EMSCRIPTEN_KEEPALIVE
-int get_result_size() {
-  return result[1];
-}
-
-EMSCRIPTEN_KEEPALIVE
-uint8_t* create_buffer(int width, int height) {
-  return malloc(width * height * 4 * sizeof(uint8_t));
-}
-
-EMSCRIPTEN_KEEPALIVE
-void destroy_buffer(uint8_t* p) {
-  free(p);
+int main()
+{
+  EM_ASM(webpLoaded());
 }
